1f66fd754ff187bd2aecec65600d81961eff4a5b
Xenoslogic <sdv666999@gmail.com>
2023-11-08 11:50:07 +0300

    bookReader.task.js done

---
+++ dom/book-reader/task.js
@@ -0,0 +1,20 @@
+const fontSize = Array.from(document.querySelectorAll('.font-size'));
+const book = document.getElementById('book');
+
+
+for (let i = 0; i < fontSize.length; i++) {
+  fontSize[i].onclick = function () {
+    let active = fontSize.findIndex((item, idx) => item.classList.contains('font-size_active'));
+    fontSize[active].classList.remove('font-size_active');
+    fontSize[i].classList.add('font-size_active');
+
+    if (fontSize[i].classList.contains('font-size_small')) {
+      book.classList.add('book_fs-small');
+    } else if (fontSize[i].classList.contains('font-size_big')) {
+      book.classList.add('book_fs-big');
+    } else {
+      book.className = 'book';
+    };
+    return false;
+  }
+}
